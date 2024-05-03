15d667344b85b43fb6e8a2599e9328ed12aba10e
최현기 <gusrl123456@gmail.com>
2024-05-03 13:06:00 +0900

    feat: 회원 기능 일부 구현, 방 api 구현 S10P31A305-305

--- backend/mrc/src/main/java/com/alpaca/mrc/domain/record/repository/RecordRepository.java
+++ backend/mrc/src/main/java/com/alpaca/mrc/domain/record/repository/RecordRepository.java
@@ -2,6 +2,8 @@ package com.alpaca.mrc.domain.record.repository;
 
 import com.alpaca.mrc.domain.record.entity.Record;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaRepositories(basePackageClasses = RecordRepository.class)
 public interface RecordRepository extends JpaRepository<Record, Long> {
 }
